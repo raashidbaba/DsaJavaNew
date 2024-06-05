949b7a71225628b544c8fe9ff5bfc676f39c1298
raashid <raashidbaba849@gmail.com>
2024-06-05 20:55:04 +0530

    3d commit

--- src/Arrays/DiffSizeColum.java
+++ src/Arrays/DiffSizeColum.java
@@ -1,3 +1,5 @@
+package Arrays;
+
 public class DiffSizeColum {
     public static void main(String[] args) {
         int[][] arr = {
--- src/Arrays/MaxValueInArray.java
+++ src/Arrays/MaxValueInArray.java
@@ -1,3 +1,5 @@
+package Arrays;
+
 public class MaxValueInArray {
     public static void main(String[] args) {
         int[] arr = {1,3,5,2,66};
