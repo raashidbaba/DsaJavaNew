bbbc33e06876b31b2b97ba5cff3495a972443e9e
raashid <raashidbaba849@gmail.com>
2024-06-08 14:59:11 +0530

    check commit 2

--- src/Arrays/Leetcode1Arrays.java
+++ src/Arrays/Leetcode1Arrays.java
@@ -4,6 +4,7 @@ public class Leetcode1Arrays {
     public static void main(String[] args) {
         int[] nums = {2,7,11,15};
         int target = 9;
+        System.out.println("change");
     }
 
 
