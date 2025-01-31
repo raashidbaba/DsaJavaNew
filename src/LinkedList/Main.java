bc9f671b12b5eeef0f0d7103f19e097c67668b63
raashid <raashidbaba849@gmail.com>
2025-01-31 23:08:30 +0530

    -linked list commit 2

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -4,18 +4,27 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LL list = new LL();
-        list.insertFirst(2);
-        list.insertFirst(7);
-        list.insertFirst(9);
-        list.insertFirst(11);
-        list.insertAtLast(99);
-        list.insertAtIndex(277,3);
-        System.out.println( list.deleteFirst());
-        list.display();
+//        LL list = new LL();
+//        list.insertFirst(2);
+//        list.insertFirst(7);
+//        list.insertFirst(9);
+//        list.insertFirst(11);
+//        list.insertAtLast(99);
+//        list.insertAtIndex(277,3);
+//        System.out.println( list.deleteFirst());
+//        list.display();
+//
+//        System.out.println(list.deleteLast());
+//        list.display();
 
-        System.out.println(list.deleteLast());
-        list.display();
+
+        DoubleLinkedLIst dll = new DoubleLinkedLIst();
+        dll.insertFirst(1);
+        dll.insertFirst(7);
+        dll.insertFirst(9);
+        dll.insertFirst(11);
+        dll.insertAtLast(99);
+        dll.display();
 
     }
 }
