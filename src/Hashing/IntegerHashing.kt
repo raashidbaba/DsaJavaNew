98d2ecd81c5027e3c22c458e9a0d03f7508d5721
raashid <raashidbaba849@gmail.com>
2025-04-25 19:12:26 +0530

    -hashing,arrays

---
+++ src/Hashing/IntegerHashing.kt
@@ -0,0 +1,35 @@
+package Hashing
+
+
+fun main() {
+    val arr = intArrayOf(1, 3, 2, 1, 3)
+    val queries = intArrayOf(1, 4, 2, 3, 12)
+    val freq = IntArray(13)
+
+    //pre-computation
+    //hashing
+    for (i in arr){
+        freq[i]++
+    }
+
+
+    //fetching
+    for (q in queries){
+        val count = if (q in queries.indices){
+            freq[q]
+        }else{
+            0
+        }
+        print("count ${count}")
+    }
+
+
+
+
+
+
+
+
+}
+
+
