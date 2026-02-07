f6dab5a1b8c2869793e91ef3546d834de8062a00
raashid <raashidbaba849@gmail.com>
2026-02-07 20:25:02 +0530

    stack neetcode150

--- src/neetcode150/CarFleet.kt
+++ src/neetcode150/CarFleet.kt
@@ -1,4 +1,29 @@
 package neetcode150
 
-class CarFleet {
+
+fun main(){
+
+}
+
+
+
+fun CarFleet(target: Int, position: IntArray, speed: IntArray):Int{
+    val stack = mutableListOf<Double>()
+    val cars = position.zip(speed)
+        .sortedByDescending { it.first }
+
+    for ((pos, spd) in cars) {
+
+        val time = (target - pos).toDouble() / spd
+
+
+        if (stack.isEmpty() || time > stack.last()) {
+            stack.add(time)
+        }
+
+    }
+
+    return stack.size
+
+
 }
