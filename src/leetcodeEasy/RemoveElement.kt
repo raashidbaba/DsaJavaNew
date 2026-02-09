e6abb9f72ff9b3968e0df3ea9b2b2aab85aeb038
raashid <raashidbaba849@gmail.com>
2026-02-09 21:01:48 +0530

    leetcode easy

--- src/leetcodeEasy/RemoveElement.kt
+++ src/leetcodeEasy/RemoveElement.kt
@@ -1,4 +1,19 @@
 package leetcodeEasy
 
+
+
 class RemoveElement {
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+        var k = 0
+
+        for (i in nums.indices) {
+
+            if (nums[i] != `val`) {
+                nums[k] = nums[i]
+                k++
+            }
+        }
+
+        return k
+    }
 }
